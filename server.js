5e445c221a1294309e3998a8bbd22acaeae5e774
Alexander Mattner <alexander.mattner@otto-schneider.de>
2021-04-18 16:40:18 +0200

    server.js - express, body-parser, cors set up | spin up server | GET & POST route set up

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {};  // projectData = []; array is also possible, both are objects in JS 
 
 // Require Express to run server and routes
 const express = require('express');     // code by Alex hinzugefügt am 12.04
@@ -30,18 +30,12 @@ const port = 8000;
 
 // spin up the server
 const server = app.listen(port, listening);                          // code by Alex hinzugefügt am 13.04
-//const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
 
 // callback to debug server
     function listening() {                                          // code by Alex hinzugefügt am 13.04
         console.log('server is running');
         console.log(`server is running on localhost: ${port}`)
     }
-
-
-
-
-
 
 
     
@@ -53,3 +47,83 @@ app.get('/', function(req, res) {
 app.get('/', function(request, response) {
     response.send(projectData);
 })
+
+
+
+//Add a GET rout that returns projectData
+app.get('/allDataSendFromServer', allData());
+
+    function allData(request, response) {
+        response.send(projectData);
+        //???   projectData = [];
+        //???   return projectData
+
+    }
+
+
+
+//Add a POST route that adds incoming data to projectData
+app.post('/addData', addIncomingDataToProjectData);
+
+    function addIncomingDataToProjectData(request, response) {
+        console.log(req.body);
+        newEntryToProjectData = {
+            date: req.body.date,
+            temp: req.body.temp,
+            content: req.body.content
+         }
+
+         projectData.push(newEntryToProjectData);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//example of Api call
+//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=523847e1c3ae8b74f9b3303c447bab38
+
+//API variables
+/*const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID='
+const apiKey = '523847e1c3ae8b74f9b3303c447bab38';
+
+const getData = async (baseURL, apiKey) => {
+
+    const res = await fetch(baseURL+apiKey);
+
+    try {
+        const data = await.response.json();
+        console.log(newData);
+        //return newData
+    } 
+
+    catch (error) {
+        console.log('error', error);
+    }
+}
+
+ 
+
+
+
+
+
+
+const response = await fetch(baseURL+apiKey) {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+}) */
--- website/app.js
+++ website/app.js
@@ -3,3 +3,37 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+
+//example of Api call
+//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=523847e1c3ae8b74f9b3303c447bab38
+
+//API variables
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID='
+const apiKey = '523847e1c3ae8b74f9b3303c447bab38';
+
+const getData = async (baseURL, apiKey) => {
+
+    const res = await fetch(baseURL+apiKey);
+
+    try {
+        const data = await response.json();
+        console.log(newData);
+        //return newData
+    } 
+
+    catch (error) {
+        console.log('error', error);
+    }
+}
+
+/* const response = await fetch(baseURL+apiKey) {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+}) */
